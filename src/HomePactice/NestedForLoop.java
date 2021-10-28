402c97cad5d26c664bd6bdd70a3a8e0914c384eb
Hiral Kalani <hiralpanara@yahoo.com>
2021-10-28 20:36:44 +0100

    Nested For Loop example
    Signed-off-by: Hiral Kalani <hiralpanara@yahoo.com>

---
+++ src/HomePactice/NestedForLoop.java
@@ -0,0 +1,12 @@
+package HomePactice;
+
+public class NestedForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int a = 2; a <= 5; a++) {
+                System.out.println(i + " " + a);
+            }
+        }
+    }
+
+}
